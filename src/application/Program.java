8d66d0b112bb1e27165a76265bdf0283197bb9ef
Paulo1985-git <paulomattos.vv@gmail.com>
2023-02-26 18:09:29 -0300

    implemented JOptionPAne

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,26 @@
 package application;
 
+import javax.swing.JOptionPane;
+
 import entities.Aluno;
 
 public class Program {
 
 	public static void main(String[] args) {
-
+		
+		String nome = JOptionPane.showInputDialog("Qual o nome do Aluno? ");
+		String idade = JOptionPane.showInputDialog("Idade: ");
+		String nascimento = JOptionPane.showInputDialog("Data de nascimento:");
+		String rg = JOptionPane.showInputDialog("Registro Geral: ");
+		String mae = JOptionPane.showInputDialog("Nome da mãe: ");
+		String pai = JOptionPane.showInputDialog("Nome do pai: ");
+		String matricula = JOptionPane.showInputDialog("Matrícula: ");
+		String serie = JOptionPane.showInputDialog("Série");
+		String escola = JOptionPane.showInputDialog("Escola: ");
+		String nota1
+		
 		Aluno aluno1 = new Aluno();
-		aluno1.setNome("João da Silva");
+		aluno1.setNome(nome);
 		aluno1.setIdade(50);
 		aluno1.setDataNascimento("18/10/1977");
 		aluno1.setRegistroGeral("444.555.1-0");
@@ -24,8 +37,8 @@ public class Program {
 		System.out.println("Nome é: " + aluno1.getNome());
 		System.out.println("Idade é: " + aluno1.getIdade());
 		System.out.println("Nascimento: " + aluno1.getDataNascimento());
-		
 		System.out.printf("Média da nota é: %.2f%n", aluno1.getMediaNota());
+		System.out.println("Resultado = " + aluno1.getAlunoAprovado());
 		
 		Aluno aluno2 = new Aluno();
 		aluno2.setNome("Alex Santos");
