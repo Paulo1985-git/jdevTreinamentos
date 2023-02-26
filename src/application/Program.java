9bf930db90f30c24f4459df3fe92f6b6b8745c28
Paulo1985-git <paulomattos.vv@gmail.com>
2023-02-26 19:27:12 -0300

    JDEV Treinamentos01

---
+++ src/application/Program.java
@@ -0,0 +1,21 @@
+package application;
+
+import entities.Aluno;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Aluno aluno1 = new Aluno();
+
+		Aluno aluno2 = new Aluno();
+
+		Aluno aluno3 = new Aluno();
+		
+		Aluno aluno4 = new Aluno("Maria");
+		
+		Aluno aluno5 = new Aluno("Pedro", 50);
+				
+	}
+
+}
---
+++ src/entities/Aluno.java
@@ -0,0 +1,29 @@
+package entities;
+
+public class Aluno {
+
+	String nome;
+	int idade;
+	String dataNascimento;
+	String registroGeral;
+	String numeroCpf;
+	String nomeMae;
+	String nomePai;
+	String dataMatricula;
+	String nomeEscola;
+	String serieMatriculado;
+
+	//diferentes tipos de construtores para criação de objetos!!!
+	public Aluno() {
+
+	}
+
+	public Aluno(String nomePadrao) {
+		nome = nomePadrao;
+	}
+	
+	public Aluno (String nomePadrao, int idadePadrao) {
+		nome = nomePadrao;
+		idade = idadePadrao;
+	}
+}
