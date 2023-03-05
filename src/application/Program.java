0f7885a2fa32a6d381cbe467a35dfffca0d01f9d
Paulo1985-git <paulomattos.vv@gmail.com>
2023-03-04 21:45:51 -0300

    Class updating

--- src/application/Program.java
+++ src/application/Program.java
@@ -12,9 +12,9 @@ public class Program {
 		String idade = JOptionPane.showInputDialog("Idade: ");
 		String nascimento = JOptionPane.showInputDialog("Data de nascimento:");
 		String rg = JOptionPane.showInputDialog("Registro Geral: ");
-		String mae = JOptionPane.showInputDialog("Nome da mãe: ");
+		String mae = JOptionPane.showInputDialog("Nome da mae: ");
 		String pai = JOptionPane.showInputDialog("Nome do pai: ");
-		String matricula = JOptionPane.showInputDialog("Matrícula: ");
+		String matricula = JOptionPane.showInputDialog("Matricula: ");
 		String serie = JOptionPane.showInputDialog("Série");
 		String escola = JOptionPane.showInputDialog("Escola: ");
 		String disciplina1 = JOptionPane.showInputDialog("Disciplina 1: ");
@@ -45,10 +45,12 @@ public class Program {
 		aluno1.setNota3(Double.parseDouble(nota3));
 		aluno1.setNota4(Double.parseDouble(nota4));
 
-		System.out.println("Nome é: " + aluno1.getNome());
-		System.out.println("Idade é: " + aluno1.getIdade());
+		System.out.println("Nome: " + aluno1.getNome());
+		System.out.println("Idade: " + aluno1.getIdade());
 		System.out.println("Nascimento: " + aluno1.getDataNascimento());
-		System.out.printf("Média da nota é: %.2f%n", aluno1.getMediaNota());
+		System.out.printf("Media da nota: %.2f%n", aluno1.getMediaNota());
+		System.out.println("Nome da Mae: " + aluno1.getNomeMae());
+		System.out.println("Mome do Pai: " + aluno1.getNomePai());
 
 	}
 }
--- src/entities/Aluno.java
+++ src/entities/Aluno.java
@@ -1,14 +1,7 @@
 package entities;
 
-public class Aluno {
+public class Aluno extends Pessoa{
 
-	private String nome;
-	private int idade;
-	private String dataNascimento;
-	private String registroGeral;
-	private String numeroCpf;
-	private String nomeMae;
-	private String nomePai;
 	private String dataMatricula;
 	private String nomeEscola;
 	private String serieMatriculado;
@@ -83,62 +76,6 @@ public class Aluno {
 
 	public void setNota4(double nota4) {
 		this.nota4 = nota4;
-	}
-
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-	public int getIdade() {
-		return idade;
-	}
-
-	public void setIdade(int idade) {
-		this.idade = idade;
-	}
-
-	public String getDataNascimento() {
-		return dataNascimento;
-	}
-
-	public void setDataNascimento(String dataNascimento) {
-		this.dataNascimento = dataNascimento;
-	}
-
-	public String getRegistroGeral() {
-		return registroGeral;
-	}
-
-	public void setRegistroGeral(String registroGeral) {
-		this.registroGeral = registroGeral;
-	}
-
-	public String getNumeroCpf() {
-		return numeroCpf;
-	}
-
-	public void setNumeroCpf(String numeroCpf) {
-		this.numeroCpf = numeroCpf;
-	}
-
-	public String getNomeMae() {
-		return nomeMae;
-	}
-
-	public void setNomeMae(String nomeMae) {
-		this.nomeMae = nomeMae;
-	}
-
-	public String getNomePai() {
-		return nomePai;
-	}
-
-	public void setNomePai(String nomePai) {
-		this.nomePai = nomePai;
 	}
 
 	public String getDataMatricula() {
