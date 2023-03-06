ed5dc7a1499a3205929447eca01151cdfba8df73
Paulo1985-git <paulomattos.vv@gmail.com>
2023-03-06 15:33:50 -0300

    clean project for to study

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,56 +1,8 @@
 package application;
 
-import javax.swing.JOptionPane;
-
-import entities.Aluno;
-
 public class Program {
 
 	public static void main(String[] args) {
-
-		String nome = JOptionPane.showInputDialog("Qual o nome do Aluno? ");
-		String idade = JOptionPane.showInputDialog("Idade: ");
-		String nascimento = JOptionPane.showInputDialog("Data de nascimento:");
-		String rg = JOptionPane.showInputDialog("Registro Geral: ");
-		String mae = JOptionPane.showInputDialog("Nome da mae: ");
-		String pai = JOptionPane.showInputDialog("Nome do pai: ");
-		String matricula = JOptionPane.showInputDialog("Matricula: ");
-		String serie = JOptionPane.showInputDialog("Série");
-		String escola = JOptionPane.showInputDialog("Escola: ");
-		String disciplina1 = JOptionPane.showInputDialog("Disciplina 1: ");
-		String nota1 = JOptionPane.showInputDialog("Nota 1: ");
-		String disciplina2 = JOptionPane.showInputDialog("Disciplina 2: ");
-		String nota2 = JOptionPane.showInputDialog("Nota 2: ");
-		String disciplina3 = JOptionPane.showInputDialog("Disciplina 3: ");
-		String nota3 = JOptionPane.showInputDialog("Nota 3: ");
-		String disciplina4 = JOptionPane.showInputDialog("Disciplina 4: ");
-		String nota4 = JOptionPane.showInputDialog("Nota 4: ");
-
-		Aluno aluno1 = new Aluno();
-		aluno1.setNome(nome);
-		aluno1.setIdade(Integer.parseInt(idade));
-		aluno1.setDataNascimento(nascimento);
-		aluno1.setRegistroGeral(rg);
-		aluno1.setNomeMae(mae);
-		aluno1.setNomePai(pai);
-		aluno1.setDataMatricula(matricula);
-		aluno1.setSerieMatriculado(serie);
-		aluno1.setNomeEscola(escola);
-		aluno1.setDisciplina1(disciplina1);
-		aluno1.setDisciplina2(disciplina2);
-		aluno1.setDisciplina3(disciplina3);
-		aluno1.setDisciplina4(disciplina4);
-		aluno1.setNota1(Double.parseDouble(nota1));
-		aluno1.setNota2(Double.parseDouble(nota2));
-		aluno1.setNota3(Double.parseDouble(nota3));
-		aluno1.setNota4(Double.parseDouble(nota4));
-
-		System.out.println("Nome: " + aluno1.getNome());
-		System.out.println("Idade: " + aluno1.getIdade());
-		System.out.println("Nascimento: " + aluno1.getDataNascimento());
-		System.out.printf("Media da nota: %.2f%n", aluno1.getMediaNota());
-		System.out.println("Nome da Mae: " + aluno1.getNomeMae());
-		System.out.println("Mome do Pai: " + aluno1.getNomePai());
 
 	}
 }
--- src/entities/Pessoa.java
+++ src/entities/Pessoa.java
@@ -1,7 +1,7 @@
 package entities;
 
 public class Pessoa {
-	
+
 	private String nome;
 	private int idade;
 	private String dataNascimento;
@@ -9,49 +9,5 @@ public class Pessoa {
 	private String numeroCpf;
 	private String nomeMae;
 	private String nomePai;
-	public String getNome() {
-		return nome;
-	}
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-	public int getIdade() {
-		return idade;
-	}
-	public void setIdade(int idade) {
-		this.idade = idade;
-	}
-	public String getDataNascimento() {
-		return dataNascimento;
-	}
-	public void setDataNascimento(String dataNascimento) {
-		this.dataNascimento = dataNascimento;
-	}
-	public String getRegistroGeral() {
-		return registroGeral;
-	}
-	public void setRegistroGeral(String registroGeral) {
-		this.registroGeral = registroGeral;
-	}
-	public String getNumeroCpf() {
-		return numeroCpf;
-	}
-	public void setNumeroCpf(String numeroCpf) {
-		this.numeroCpf = numeroCpf;
-	}
-	public String getNomeMae() {
-		return nomeMae;
-	}
-	public void setNomeMae(String nomeMae) {
-		this.nomeMae = nomeMae;
-	}
-	public String getNomePai() {
-		return nomePai;
-	}
-	public void setNomePai(String nomePai) {
-		this.nomePai = nomePai;
-	}
-	
-	
 
 }
